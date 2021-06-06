d111df17c2ee134f8eb143b85b5df5c9b094d35c
Shivam Mishra <shivam07.mt@gmail.com>
2021-06-06 17:01:06 +0530

    subscribing the state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import store from './store.js';
 import { bugAdded } from './actions.js';
 
-console.log("hello hmtl")
+store.subscribe(() => {
+    console.log("store state changed ", store.getState())
+})
 
 store.dispatch(bugAdded('bug 1'))
-
-console.log(store.getState())
