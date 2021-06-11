487221fe013cd6ade4fc013a36fc0f83b8d0c748
Shivam Mishra <shivam07.mt@gmail.com>
2021-06-11 21:54:41 +0530

    added new reducer projects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import store from './store/store';
-import * as actions from './store/bugs';
+import * as actions from './store/projects';
 
 store.subscribe(() => {
     console.log(store.getState())
 })
 
-store.dispatch(actions.bugAdded({ description: 'Bug 1' }))
-store.dispatch(actions.bugAdded({ description: 'Bug 2' }));
-store.dispatch(actions.bugResolved({ id: 1 }))
-store.dispatch(actions.bugRemoved({ id: 1 }));
+store.dispatch(actions.projectAdded({ name: 'Project 1' }))
+store.dispatch(actions.projectAdded({ name: 'Project 2' }));
+
