b6bbae2fb98aa9af6ac5c021e83082d9b0484601
Shivam Mishra <shivam07.mt@gmail.com>
2021-06-12 20:08:31 +0530

    selcting specific bugs from state

--- src/index.js
+++ src/index.js
@@ -10,4 +10,15 @@ store.dispatch(projectAdded({ name: 'Project 1' }));
 store.dispatch(projectAdded({ name: 'Project 2' }));
 
 store.dispatch(actions.bugAdded({ description: 'Bug 1' }))
+store.dispatch(actions.bugAdded({ description: 'Bug 2' }))
+store.dispatch(actions.bugAdded({ description: 'Bug 3' }))
+store.dispatch(actions.bugAdded({ description: 'Bug 4' }))
+store.dispatch(actions.bugResolved({ id: 1 }))
+
+// implementing a selector to get the unresolved bugs
+
+const unresolvedBugs = store.getState().entities.bugs
+    .filter(bug => !bug.resolved)
+
+console.log(unresolvedBugs)
 
